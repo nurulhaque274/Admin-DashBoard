e1f33b3b011ff44ef7050e144d04097ada4dce4d
Nurul Haque <nurulhaque274@gmail.com>
2021-11-02 12:16:06 +0530

    Initial commit

---
+++ javascript.js
@@ -0,0 +1,7 @@
+// var hamburge = document.querySelector('.hamburge');
+// var sidebar =;
+
+function toggle(){
+    document.getElementsByClassName('sidebar_left').classList.toggle('active');
+    console.log("clicked");
+}
